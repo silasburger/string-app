ab8e090f38b095cdb4d0e7bf865c0cb9caef2d51
Silas Burger <ybsilas@gmail.com>
2019-02-16 08:20:26 +0200

    flexbox layout to string-page

--- frontend/app/components/PostList/ListWrapper.js
+++ frontend/app/components/PostList/ListWrapper.js
@@ -4,7 +4,6 @@ export default styled.div`
   width: 70%;
   max-width: 500px;
   border: solid 2px #003366;
-  margin: 0 auto;
   height: 500px;
   overflow-y: scroll;
   overflow-x: hidden;
--- frontend/app/components/StringPageView/index.js
+++ frontend/app/components/StringPageView/index.js
@@ -10,20 +10,30 @@ import styled from 'styled-components';
 import { Helmet } from 'react-helmet';
 
 import PostList from 'components/PostList';
+import StringFormContainer from 'containers/StringFormContainer/Loadable';
 import { FormattedMessage } from 'react-intl';
 import messages from './messages';
+
+const View = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
 /* eslint-disable react/prefer-stateless-function */
 class StringPageView extends React.Component {
   render() {
     return (
-      <div>
+      <View>
         <Helmet>
           <title>String Page</title>
           <meta name="String Page" content="This page shows all the strings!" />
         </Helmet>
         <FormattedMessage {...messages.header} />
         <PostList posts={this.props.posts} />
-      </div>
+        <StringFormContainer />
+      </View>
     );
   }
 }
